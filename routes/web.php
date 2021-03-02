9234e0aa6f056db6e7ecee7c4bb3c9c8d61ebfff
Mutombo <mgtshomba@gmail.com>
2021-03-02 05:18:16 +0200

    created the user profile route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,7 @@ Route::get('/admin/create', [AdminController::class, 'create'])->name('admin.cre
 Route::get('/admin/{post}/edit', [AdminController::class, 'edit'])->Middleware('can:update,post')->name('admin.edit');
 Route::put('/admin/{post}', [AdminController::class, 'update'])->Middleware('can:update,post')->name('admin.update');
 Route::delete('/admin/{post}', [AdminController::class, 'destroy'])->name('admin.delete');
+
+Route::get('/admin/user/profile/{user}', [UserController::class, 'show'])->name('user.profile');
 
 Route::get('/post/{id}', [PostController::class, 'show'])->name('post');
